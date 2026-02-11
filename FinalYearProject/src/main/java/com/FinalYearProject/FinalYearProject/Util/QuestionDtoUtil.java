8645e724d4b22da5959d476a92a84f4583e75c26
Hassan Rafai <hassanrafai1707@gmail.com>
2026-02-11 22:01:06 +0530

    updated QuestionToQuestionDto to make it morden

--- FinalYearProject/src/main/java/com/FinalYearProject/FinalYearProject/Util/QuestionDtoUtil.java
+++ FinalYearProject/src/main/java/com/FinalYearProject/FinalYearProject/Util/QuestionDtoUtil.java
@@ -5,13 +5,13 @@ import com.FinalYearProject.FinalYearProject.Domain.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class QuestionDtoUtil {
 
+    private QuestionDtoUtil() {};
     public static QuestionDTO QuestionToQuestionDto(Question question){
         return new QuestionDTO(
                 question.getId(),
@@ -28,28 +28,31 @@ public class QuestionDtoUtil {
         if (questions.isEmpty()){
             throw new IllegalArgumentException("the question passed in this method can not be null");
         }
-        List<QuestionDTO> temp=new ArrayList<>();
-        for (Question question:questions){
-            temp.add(
-                    new QuestionDTO(
-                            question.getId(),
-                            question.getSubjectName(),
-                            question.getQuestionMarks(),
-                            question.getMappedCO(),
-                            question.getSubjectCode(),
-                            question.getCognitiveLevel(),
-                            question.getQuestionBody()
-                    )
-            );
-        }
-        return temp;
+        return questions.stream().map(question -> new QuestionDTO(
+                question.getId(),
+                question.getSubjectName(),
+                question.getQuestionMarks(),
+                question.getMappedCO(),
+                question.getSubjectCode(),
+                question.getCognitiveLevel(),
+                question.getQuestionBody()
+        )).collect(Collectors.toList());
     }
 
     public static PageImpl<QuestionDTO> questionToQuestionDTO_Paged(Page<Question> questionPage, int pageNo, int size){
+        if (questionPage.isEmpty()||questionPage.getContent().isEmpty()){
+            throw new IllegalArgumentException("the question passed in this method can not be null");
+        }
         return new PageImpl<>(
-                listOfQuestionToQuestionDto(
-                        questionPage.getContent()
-                ),
+                questionPage.getContent().stream().map(question -> new QuestionDTO(
+                        question.getId(),
+                        question.getSubjectName(),
+                        question.getQuestionMarks(),
+                        question.getMappedCO(),
+                        question.getSubjectCode(),
+                        question.getCognitiveLevel(),
+                        question.getQuestionBody()
+                )).collect(Collectors.toList()),
                 PageRequest.of(pageNo,size),
                 questionPage.getTotalElements()
         );
