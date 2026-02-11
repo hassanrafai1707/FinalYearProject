22a1984f94f238775b9bfee636c84d3b0ea531f2
Hassan Rafai <hassanrafai1707@gmail.com>
2026-02-11 21:48:05 +0530

    made a new util class UserDtoUtil to make user into user dto for security resions

---
+++ FinalYearProject/src/main/java/com/FinalYearProject/FinalYearProject/DTO/UserDto/UserDto.java
@@ -0,0 +1,83 @@
+package com.FinalYearProject.FinalYearProject.DTO.UserDto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+    private Long Id;
+
+    private String name;
+
+    private String email;
+
+    private String role;
+
+    private boolean is_enable;
+
+    private boolean locked;
+
+    private boolean expired;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public boolean isExpired() {
+        return expired;
+    }
+
+    public void setExpired(boolean expired) {
+        this.expired = expired;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public boolean isIs_enable() {
+        return is_enable;
+    }
+
+    public void setIs_enable(boolean is_enable) {
+        this.is_enable = is_enable;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ FinalYearProject/src/main/java/com/FinalYearProject/FinalYearProject/Util/UserDtoUtil.java
@@ -0,0 +1,55 @@
+package com.FinalYearProject.FinalYearProject.Util;
+
+import com.FinalYearProject.FinalYearProject.DTO.UserDto.UserDto;
+import com.FinalYearProject.FinalYearProject.Domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserDtoUtil {
+
+    private UserDtoUtil(){};
+
+    public static UserDto UserToUserDto(User user){
+        return new UserDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole(),
+                user.isIs_enable(),
+                user.isLocked(),
+                user.isExpired()
+        );
+    }
+
+    public static List<UserDto> listOfUserToUserDto(List<User> users){
+        return users.stream().map(user -> new UserDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole(),
+                user.isIs_enable(),
+                user.isLocked(),
+                user.isExpired()
+        )).collect(Collectors.toList());
+    }
+
+    public static PageImpl<UserDto> UserToUserDtoPaged(Page<User> users, int pageNo,int size){
+        return new PageImpl<>(
+                users.getContent().stream().map(user -> new UserDto(
+                        user.getId(),
+                        user.getName(),
+                        user.getEmail(),
+                        user.getRole(),
+                        user.isIs_enable(),
+                        user.isLocked(),
+                        user.isExpired()
+                )).collect(Collectors.toList()),
+                PageRequest.of(pageNo,size),
+                users.getTotalElements()
+        );
+    }
+}
